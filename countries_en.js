7223ac229f0097ec0106de3d944648104885b378
superlot <mintolmax@gmail.com>
2020-03-29 14:18:53 +0300

    COVID
    COVID

---
+++ countries_en.js
@@ -0,0 +1,7 @@
+// Copyright 2008 Google Inc.
+// All Rights Reserved.
+
+(function() {
+  var results = {"east timor":  "ukraine": "UA", "melanesia": "UN054", };
+  gviz.util.RegionCoder.dictionaryReady('countries_en', results);
+})();
